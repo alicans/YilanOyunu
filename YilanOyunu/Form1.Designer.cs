09606311fd9f4f4bd668d4c2ff25efe787244093
alicans <alican@outlook.com>
2023-09-29 14:29:54 +0300

    Add project files.

---
+++ YilanOyunu/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace YilanOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlSaha = new Panel();
+            SuspendLayout();
+            // 
+            // pnlSaha
+            // 
+            pnlSaha.BackColor = SystemColors.ActiveCaption;
+            pnlSaha.Location = new Point(12, 12);
+            pnlSaha.Name = "pnlSaha";
+            pnlSaha.Size = new Size(500, 500);
+            pnlSaha.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(527, 524);
+            Controls.Add(pnlSaha);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlSaha;
+    }
+}
---
+++ YilanOyunu/Form1.cs
@@ -0,0 +1,10 @@
+namespace YilanOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
